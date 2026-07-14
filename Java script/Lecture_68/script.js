b2a481e9d68d7b9c7ff012bc94a69d7559627564
unknown <snjoshi867@gmail.com>
2026-07-14 12:28:27 +0530

    Selecting Elements by ID and Classes

---
+++ Java script/Lecture_68/script.js
@@ -0,0 +1,18 @@
+console.log("soham")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor ="red"
+
+// let c = document.getElementById("red")
+// console.log(c)
+
+// c.style.backgroundColor="red"
+
+ //document.querySelector(".box").style.backgroundColor = "green"
+
+document.querySelectorAll(".box").forEach(e => {
+    e.style.backgroundColor = "red"
+})
+
