3f50d06c88a85ae65be571e6ca88a3bf81c195bf
unknown <snjoshi867@gmail.com>
2026-07-17 08:43:58 +0530

    Events, Event Bubbling, setInterval and setTimeout

--- Java script/Lecture_74/script.js
+++ Java script/Lecture_74/script.js
@@ -1,31 +1,17 @@
-function creatCard(title,cName,views,monthsOld,duration,thumbnail){
-    let viewstr
-    if(views<1000000){
-          viewstr=views/1000 + "K"
-    }
-    else if (views>1000000){
-         viewstr=views/1000000 + "M"
-    }
-    else{
-        viewstr=views/1000 + "K"
-    }
-   
-    let html =`<div class="card">
-            <div class="image">
-                <img src="${thumbnail}">
-                <div class = "capsule">${duration}</div>
-            </div>
-            <div class="text">
-                <h1>${title}</h1>
-                <p>${cName}. ${viewstr} views . ${monthsOld} monts ago</p>
-            </div>
+let button = document.getElementById("btn")
 
-        </div>`
-    
- document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
 
-}
 
-creatCard("Introduction to backend | sigma web dev ", "Code with mohan" , 560000, 7,"31:32", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4pqAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAwWOixJVrKLFindK92kYMgTcQbw")
-creatCard("Introduction to backend | sigma web dev ", "Code with mohan" , 560000, 7,"31:32", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4pqAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAwWOixJVrKLFindK92kYMgTcQbw")
-creatCard("Introduction to backend | sigma web dev ", "Code with mohan" , 560000, 7,"31:32", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4pqAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAwWOixJVrKLFindK92kYMgTcQbw")
+button.addEventListener("click", ()=>{
+    document.querySelector(".box").innerHTML = "<b>OMG what is this !!</b> "
+    console.log("Content has been changed")
+})
+
+button.addEventListener("dblclick",()=> {
+    window.location.href="https://www.google.com"
+})
+button.addEventListener("keydown",(e)=> {
+    console.log(e ,e.key , e.keyCode)
+})
+
+
