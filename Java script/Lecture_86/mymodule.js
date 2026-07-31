148d3b47e4b88c451509186c11551a5412243453
unknown <snjoshi867@gmail.com>
2026-07-31 23:25:36 +0530

    A change

--- Java script/Lecture_86/mymodule.js
+++ Java script/Lecture_86/mymodule.js
@@ -1,8 +1,8 @@
-export const a = 1 // named export
-export const b = 2 // named export
-export const c = 3 // named export
-export const d = 4 // named export
-export const e = 5 // named export
+export const a = 1 
+export const b = 2 
+export const c = 3 
+export const d = 4 
+export const e = 5 
  
 const obj = {
     x: 5,
